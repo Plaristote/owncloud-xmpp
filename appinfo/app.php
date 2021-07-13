0a6d8f9c383e8292d58d31e88c18b8f15efe6bdd
Michael <michael@unetresgrossebite.com>
2021-07-13 15:20:16 +0200

    fix Application class never being instantiated

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,6 +1,6 @@
 <?php
-
-use OCP\Util;
+namespace OCA\XMPP\AppInfo;
+use \OCP\Util;
 
 \OCP\Util::connectHook('OC_User', 'post_login', 'OCA\XMPP\AuthHelper', 'postLogin');
 
@@ -10,3 +10,5 @@ if (\OC_User::isLoggedIn()) {
   Util::addScript("xmpp", "jsxc.bundle");
   Util::addScript("xmpp", "login");
 }
+
+$app = new Application();
